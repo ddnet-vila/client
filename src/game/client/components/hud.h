2bf011d5e46a8155a25cf82015c401f6c3f0b619
Vlad <gerdoexx@gmail.com>
2025-01-05 03:38:50 +0300

    mirror weapons of right team

--- src/game/client/components/hud.h
+++ src/game/client/components/hud.h
@@ -166,6 +166,7 @@ private:
 	void RenderTeamStatsHud(int Team);
 	void RenderStatBars();
 	STextContainerIndex m_StatBarTextContainerIndex;
+	int m_aMirroredWeaponOffset[NUM_WEAPONS];
 };
 
 #endif
