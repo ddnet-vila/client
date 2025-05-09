d561784020960c4e46a1241ab77c6152826186d8
ChillerDragon <ChillerDragon@gmail.com>
2025-05-09 16:28:18 +0200

    Add vila iam

--- src/game/version.h
+++ src/game/version.h
@@ -17,3 +17,8 @@
 extern const char *GIT_SHORTREV_HASH;
 #define GAME_NAME "DDNet"
 #endif
+
+// vila
+#define VILA_BUILD_DATE __DATE__ ", " __TIME__
+#define VILA_VERSION "0.0.1a"
+#define VILA_VERSION_NUMBER 1
